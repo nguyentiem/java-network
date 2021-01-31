de522186cd5adde1aae45b70b4fb2a1df967f342
Nguyen Van Tiem 20164039 <tiem.nv164039@sis.hust.edu.vn>
2021-01-31 17:25:31 +0700

    socket tcp

--- eclipse-workspace/network/src/network/clientTcp.java
+++ eclipse-workspace/network/src/network/clientTcp.java
@@ -19,49 +19,30 @@ import java.net.ServerSocket;
 	 
 public class clientTcp{
 	
-//	 public static void main( String[] args )
-//	    {
-//	        if( args.length < 2 )
-//	        {
-//	            System.out.println( "Usage: SimpleSocketClientExample <server> <path>" );
-//	            System.exit( 0 );
-//	        }
-//	        String server = args[ 0 ];
-//	        String path = args[ 1 ];
-//
-//	        System.out.println( "Loading contents of URL: " + server );
+	public static void main(String[] args) throws IOException {
+           
+	            Socket socket = new Socket( "192.168.1.229", 88 );
 
-//	        try
-//	        {
-//	            // Connect to the server
-//	            Socket socket = new Socket( "192.168.1.229", 88 );
-//
-//	            // Create input and output streams to read from and write to the server
-//	            //PrintStream out = new PrintStream( socket.getOutputStream() );
-//	            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
-//	          //Tạo outputStream, nối tới socket
-//	            DataOutputStream out =
-//	                    new DataOutputStream(socket.getOutputStream());
-//
-//	            out.writeBytes("Nguyen Van Tiem");
-//	            // Follow the HTTP protocol of GET <path> HTTP/1.0 followed by an empty line
-////	            out.println( "Nguyen Van Tiem" );
-////	            out.println();
-//
-//	            // Read data from the server until we finish reading the document
-//	            String line = in.readLine();
-//	            
-//	                System.out.println( line );
-//	             
-//
-//	            // Close our streams
-//	            in.close();
-//	            out.close();
-//	            socket.close();
-//	        }
-//	        catch( Exception e )
-//	        {
-//	            e.printStackTrace();
-//	        }
-//	    }
+	           try {
+	        	BufferedReader in = new BufferedReader(
+	        				 new InputStreamReader(socket.getInputStream()));
+	          
+	    		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+	  
+	    		out.writeBytes("xin chao Nguyen Van Tiem");
+	           
+	            String line = in.readLine();
+	            
+	            System.out.println( line );
+	             
+	           
+	            in.close();
+	            out.close();
+	            socket.close();
+	        }
+	        catch( Exception e )
+	        {
+	            e.printStackTrace();
+	        }
+	    }
 }
--- eclipse-workspace/network/src/network/serverTcp.java
+++ eclipse-workspace/network/src/network/serverTcp.java
@@ -8,38 +8,38 @@ public class serverTcp {
 
 
 
-
-	public static void main(String[] args) throws IOException {
-		
-		ServerSocket einSS = new ServerSocket(88);
-		
-		Socket einAS = einSS.accept();
-		
-		BufferedReader vomClient = new BufferedReader(
-		 new InputStreamReader(einAS.getInputStream()));
-		
-		BufferedWriter zumClient = new BufferedWriter(
-		 new OutputStreamWriter(einAS.getOutputStream()));
-        
-		DataOutputStream outToClient =
-        new DataOutputStream(einAS.getOutputStream());
-
-    
-   
-		String s;
-		try {
-		if((s = vomClient.readLine())!= null){
-		System.out.println(s);
-//		zumClient.write("java server send");
-//		zumClient.newLine();
-//		zumClient.flush();
-		outToClient.writeBytes("xin chao"); 
-		}
-		} catch (Exception e) { }
-		System.out.println("Client disconect!!!");
-		vomClient.close();
-		einAS.close();
-		einSS.close();
-		}
+//
+//	public static void main(String[] args) throws IOException {
+//		
+//		ServerSocket einSS = new ServerSocket(88);
+//		
+//		Socket einAS = einSS.accept();
+//		
+//		BufferedReader vomClient = new BufferedReader(
+//		 new InputStreamReader(einAS.getInputStream()));
+//		
+//		BufferedWriter zumClient = new BufferedWriter(
+//		 new OutputStreamWriter(einAS.getOutputStream()));
+//        
+//		DataOutputStream outToClient =
+//        new DataOutputStream(einAS.getOutputStream());
+//
+//    
+//   
+//		String s;
+//		try {
+//		if((s = vomClient.readLine())!= null){
+//		System.out.println(s);
+////		zumClient.write("java server send");
+////		zumClient.newLine();
+////		zumClient.flush();
+//		outToClient.writeBytes("xin chao"); 
+//		}
+//		} catch (Exception e) { }
+//		System.out.println("Client disconect!!!");
+//		vomClient.close();
+//		einAS.close();
+//		einSS.close();
+//		}
 
 }
